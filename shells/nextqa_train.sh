795f8084018c450ad5ca1ad025cc3753e23d87ce
maybelu9 <maybelu9@gmail.com>
2023-12-20 23:53:42 +0800

    Add files via upload

---
+++ shells/nextqa_train.sh
@@ -0,0 +1,20 @@
+GPU=$1
+CUDA_VISIBLE_DEVICES=$GPU python main_nextqa.py --checkpoint_dir=nextqa \
+	--dataset=nextqa \
+	--mc=5 \
+	--bnum=5 \
+	--epochs=30 \
+	--lr=0.00004 \
+	--qmax_words=30 \
+	--amax_words=38 \
+	--max_feats=32 \
+	--batch_size=64 \
+	--batch_size_val=64 \
+	--num_thread_reader=8 \
+	--mlm_prob=0 \
+	--n_layers=2 \
+	--embd_dim=512 \
+	--ff_dim=1024 \
+	--dropout=0.3 \
+	--seed=400 \
+	--save_dir='../data_loader/save_models/nextqa/mist_nextqa_lr4_4/'
